3c3b6acafcf7627f418fb2ddcae9293460a202c7
200458621 <92001185+200458621@users.noreply.github.com>
2021-11-19 20:01:35 -0500

    Add files via upload

---
+++ LAB-3.ps1
@@ -0,0 +1,1 @@
+﻿get-ciminstance -ClassName Win32_NetworkAdapterConfiguration |? {$_.ipenabled -eq "True" } | format-table -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
---
+++ LAB-5.ps1
@@ -0,0 +1,16 @@
+﻿Param ( [switch]$System , [switch]$Disks , [switch]$Network)
+
+Import-Module kanika
+
+if ($System -eq $false -and $Disks -eq $false -and $Network -eq $false) {
+	write-host "Insert kanika-System for system information."
+	write-host "Insert kanika-Disks for disks information."
+	write-host "Insert kanika-Network for network information."	
+	write-host "If no arguments found then it will display everything available here from a module."
+	write-host "==================================================================================="	
+    kanika-System;    kanika-Disks;    kanika-Network;
+}else {
+    if ($System) {  kanika-System }
+    if ($Disks)  {  kanika-Disks  }
+    if ($Network){  kanika-Network}
+}
