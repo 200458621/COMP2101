92489c881dd347ad183765df9a195563cfc81edd
200458621 <200458621@student.georgianc.on.ca>
2022-01-24 15:27:47 -0500

    test

--- LAB-3.ps1
+++
@@ -1,1 +0,0 @@
-﻿get-ciminstance -ClassName Win32_NetworkAdapterConfiguration |? {$_.ipenabled -eq "True" } | format-table -AutoSize Description, Index, IPAddress, IPSubnet, DNSDomain, DNSServerSearchOrder
